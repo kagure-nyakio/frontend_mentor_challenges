2b67a0a3c913301c41202c2aead6e836af4c9259
kagure-nyakio <kagurenyakio@gmail.com>
2023-01-11 22:17:27 +0300

    Build profile card component

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+      },
+      fontFamily: {
+        base: ['Kumbh Sans', 'sans-serif'],
+      },
+      borderWidth: {
+        'bt-width': '1px',
+      },
+      backgroundImage: {
+        'card-top': "url('images/bg-pattern-card.svg')",
+        'bg-body': [
+          "url('images/bg-pattern-top.svg')",
+          "url('images/bg-pattern-bottom.svg')",
+        ],
       },
     },
   },
